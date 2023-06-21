62d9d9776a0252d0863bf8af76ad4c2426550531
FaaDiiL <fadil.alfarhan@gmail.com>
2023-06-21 23:18:41 +0200

    Refactor button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import { ButtonStyles } from './styles'
+
+type Props = {}
+const Button: FC<Props> = ({}) => {
+  return <button className={ButtonStyles}>My Button</button>
+}
+export default Button
---
+++ components/Button/styles.ts
@@ -0,0 +1,1 @@
+export const ButtonStyles: string = 'text-primary bg-secondary'
