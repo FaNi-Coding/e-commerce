48badffc2b4c0aa681d69b31c349fcdca4878ef0
Kaayzeer <mautiagiro@gmail.com>
2023-06-29 19:17:34 +0200

    hamburgermenu crreated buit not styled

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,7 +1,11 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useEffect, useState } from 'react';
 import Section from '../Section/Section';
 import Link from 'next/link';
 import Logo from '../Icons/Logo';
+import Cart from '../Icons/Cart';
+import Hamburger from '../Icons/Hamburger';
 /* import * as styles from './styles.ts';
  */
 type Props = {};
@@ -14,10 +18,37 @@ const navLinks = [
 ];
 
 const Nav: FC<Props> = ({}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else document.body.style.overflow = 'unset';
+  }, [isOpen]);
+
+  const hamburgerMenu = () => {
+    return (
+      <>
+        {isOpen && (
+          <div className='absolute top-[9.6rem] left-0 h-[100vh] w-full z-10 opacityBlack'>
+            <div className=' flex flex-col gap bg-primary h-[75vh] w-full'>
+              ALO
+            </div>
+          </div>
+        )}
+      </>
+    );
+  };
+
   return (
-    <Section dynamicStyles='max-width-container-no-padding'>
-      <nav className='h-[9.6rem] flex justify-between items-center w-full text-white'>
+    <Section dynamicStyles='max-width-container-no-padding relative'>
+      <nav className='hidden lg:flex h-[9.6rem] justify-between items-center w-full text-white'>
         <Logo />
+
         <ul className='flex-column sm:flex gap-[3.4rem] '>
           {navLinks.map((link) => (
             <li key={link.name}>
@@ -25,10 +56,16 @@ const Nav: FC<Props> = ({}) => {
             </li>
           ))}
         </ul>
-        <ul>
-          <li>kundvagn</li>
-        </ul>
+        <Cart />
       </nav>
+      <nav className='lg:hidden flex h-[9.6rem] justify-between items-center w-full text-white px-[2.4rem] md:px-[4rem]'>
+        <div className='flex gap-[4.2rem] items-center w-full '>
+          <Hamburger onClick={toggleHamburger} />
+          <Logo />
+        </div>
+        <Cart />
+      </nav>
+      {hamburgerMenu()}
     </Section>
   );
 };
