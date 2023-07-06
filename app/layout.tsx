34a7a55a9984a7a9ac50c17c8610c5faa827f4c1
Kaayzeer <mautiagiro@gmail.com>
2023-07-06 18:06:11 +0200

    fixed background issue in hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import Footer from '@/components/Layout/Footer/Footer';
-import Header from '@/components/Layout/Header/Header';
-import { Manrope } from 'next/font/google';
+import Footer from "@/components/Layout/Footer/Footer";
+import Header from "@/components/Layout/Header/Header";
+import { Manrope } from "next/font/google";
 
-import './globals.css';
+import "./globals.css";
 
-const manrope = Manrope({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'E-commerce 2.0',
+  title: "E-commerce 2.0",
   description:
-    'E-commerce 2.0 is a demo e-commerce site built with Next.js and TypeScript.',
+    "E-commerce 2.0 is a demo e-commerce site built with Next.js and TypeScript.",
 };
- 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={manrope.className}>
-       <Header />
+        <Header />
         <main>{children}</main>
         <Footer />
       </body>
