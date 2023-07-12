e70222dac2ad102d62c10ffa81c3ae89bb7b443c
Kaayzeer <mautiagiro@gmail.com>
2023-07-12 18:25:41 +0200

    header background image glitch fixed

--- components/Layout/Header/HeaderTypes.tsx
+++ components/Layout/Header/HeaderTypes.tsx
@@ -5,23 +5,16 @@ import CoverImageComp from "@/components/UI/Image/CoverImageComp";
 import HeroImgMobile from "public/assets/home/mobile/image-header.jpg";
 import HeroImgTablet from "public/assets/home/tablet/image-header.jpg";
 import HeroImgLaptop from "public/assets/home/desktop/image-hero.jpg";
-import useMediaQuery from "@/hooks/useMediaQuery";
-import { responsive } from "@/styles/responsive";
 
 type Props = {
   slug?: string;
 };
 export const HeroHeader: FC<Props> = ({}) => {
-  const isLaptop = useMediaQuery(responsive.tabletL);
-  const isTablet = useMediaQuery(responsive.tablet);
-
-  const renderImgByScreenSize = isLaptop
-    ? HeroImgLaptop
-    : isTablet
-    ? HeroImgTablet
-    : HeroImgMobile;
   return (
     <>
+      <Article dynamicStyles="flex flex-col justify-center text-center text-white items-center  lg:max-w-[37.9rem] md:p-0 lg:items-start lg:text-left px-[2.4rem] py-[11rem] z-10 ">
+        <h1 className="h1-mobile">XX99 Mark II Headphones</h1>
+        <p className="body">
       <Article dynamicStyles="flex flex-col justify-center text-center text-white items-center  lg:max-w-[37.9rem] md:p-0 lg:items-start lg:text-left px-[2.4rem] py-[11rem] z-10 ">
         <h1 className="h1-mobile">XX99 Mark II Headphones</h1>
         <p className="body">
@@ -30,7 +23,21 @@ export const HeroHeader: FC<Props> = ({}) => {
         </p>
         <Button primary>SEE PRODUCT</Button>
       </Article>
-      <CoverImageComp src={renderImgByScreenSize} alt="hero-image" />{" "}
+      <CoverImageComp
+        dynamicWrapperStyles="hidden md:hidden lg:block"
+        src={HeroImgLaptop}
+        alt="hero-image"
+      />
+      <CoverImageComp
+        dynamicWrapperStyles="hidden md:block lg:hidden"
+        src={HeroImgTablet}
+        alt="hero-image"
+      />
+      <CoverImageComp
+        dynamicWrapperStyles="block md:hidden lg:hidden"
+        src={HeroImgMobile}
+        alt="hero-image"
+      />
     </>
   );
 };
@@ -38,6 +45,7 @@ export const HeroHeader: FC<Props> = ({}) => {
 export const CategoryHeader: FC<Props> = ({ slug }) => {
   return (
     <>
+      <Article dynamicStyles="flex justify-center text-white items-center z-10 lg:w-full">
       <Article dynamicStyles="flex justify-center text-white items-center z-10 lg:w-full">
         <h2>{slug}</h2>
       </Article>
