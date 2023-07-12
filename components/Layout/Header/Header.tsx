fb91e34d94c5a4bc66622c0e798ffb6b5afea333
Kaayzeer <mautiagiro@gmail.com>
2023-07-12 09:36:41 +0200

    fixed the header issue

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -11,32 +11,42 @@ import { HeroHeader, CategoryHeader } from "./HeaderTypes";
 type Props = {};
 const Header: FC<Props> = () => {
   const _slug = usePathname();
-  let isNotHero = !!findPathname(_slug, 1);
-  let isProduct = findPathname(_slug, 2);
+  const isHero = !findPathname(_slug, 1);
+  const isCategory = findPathname(_slug, 1);
+  const isProduct = findPathname(_slug, 2);
 
-  const isHeroStyles = !isNotHero
-    ? "h-[60rem] md:h-[72.9rem]"
-    : "md:h-[33.6rem]";
+  const headerStyles = () => {
+    if (isHero && !isProduct) {
+      return "max-width-container-header h-[60rem] md:h-[72.9rem] relative";
+    }
+    if (isCategory && !isProduct) {
+      return "max-width-container-category-header md:h-[33.6rem] relative";
+    }
+    if (isProduct) {
+      return "container mx-auto relative";
+    }
+    return "";
+  };
 
-  const globals = isNotHero
-    ? "max-width-container-category-header"
-    : "max-width-container-header";
+  const renderHeader = () => {
+    return (
+      <>
+        {isHero && !isProduct && <HeroHeader />}
+        {isCategory && !isProduct && (
+          <CategoryHeader slug={findPathname(_slug, 1)} />
+        )}
+      </>
+    );
+  };
 
   return (
     <>
       <header>
-        <Section
-          dynamicStyles={`${globals} relative ${isHeroStyles}`}
-          bgColor="bg-header"
-        >
+        <Section dynamicStyles={headerStyles()} bgColor="bg-header">
           <Nav icon={<Cart />} navHeight="h-[8.9rem] md:h-[9.6rem]">
             <MobileNav />
           </Nav>
-          {isNotHero && !isProduct ? (
-            <CategoryHeader slug={findPathname(_slug, 1)} />
-          ) : (
-            !isNotHero && !isProduct && <HeroHeader />
-          )}
+          {renderHeader()}
         </Section>
       </header>
     </>
